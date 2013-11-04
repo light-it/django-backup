f7d941a7009d46b39aec6bbe0ff55c5e74ecc746
Max_Be <light.it.zp@gmail.com>
2013-11-04 11:53:23 +0200

    fix media files importing

--- django_backup/management/commands/restore_files.py
+++ django_backup/management/commands/restore_files.py
@@ -25,19 +25,38 @@ class Command(BaseCommand):
 
         connection = self.get_connection()
 
-        backups = [i.strip() for i in connection.execute('ls %s' % (self.remote_dir))]
+        backups = [i.strip() for i in connection.sftp_client.listdir(self.remote_dir)]
+
         media_backups = filter(is_media_backup, backups)
         media_backups.sort()
         media_remote = media_backups[-1]
 
-        media_local = os.path.join(self.tempdir, media_remote)
+        media_local = os.path.join(self.dest_temp_dir, media_remote)
         connection.get(os.path.join(self.remote_dir, media_remote), media_local)
-        self.uncompress_media(media_local)
+        uncompress = self.uncompress(self.dest_temp_dir, media_local)
+        if uncompress == 0:
+            backup_media_dir = self.get_media_dir_path(self.dest_temp_dir)
+            if backup_media_dir:
+                self.replace_media(settings.MEDIA_ROOT, backup_media_dir)
 
     def get_connection(self):
         return pysftp.Connection(host=self.ftp_server, username=self.ftp_username, password=self.ftp_password)
 
-    def uncompress_media(self, file):
-        cmd = u'tar -C %s -xzf %s' % (settings.MEDIA_ROOT, file)
-        print u'\t', cmd
+    def uncompress(self, local_path, file):
+        cmd = u'tar -C %s -xzf %s' % (local_path, file)
+        print '\t', cmd
+        return os.system(cmd)
+
+    def get_media_dir_path(self, path):
+        media_path = None
+        for root, dir_names, file_names in os.walk(path):
+            for dir_name in dir_names:
+                if dir_name == 'media':
+                    media_path = os.path.join(root, dir_name)
+                    break
+        return media_path
+
+    def replace_media(self, local_path, backup_dir):
+        cmd = u'mv -f %s/* %s' % (backup_dir, local_path)
+        print '\t', cmd
         os.system(cmd)
